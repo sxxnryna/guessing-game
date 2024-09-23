aa91a17a197aeb52e331ccce2301cf15693e5040
Ryna <katrin_dr@naver.com>
2024-09-23 21:13:05 +0900

    feat: 2nd try

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -5,27 +5,25 @@ class GuessingGame {
     this.currentGuess = 0;
   }
 
-
   setRange(min, max) {
     this.min = min;
     this.max = max;
+    this.currentGuess = Math.floor((min + max) / 2); // Начальный предположение
   }
 
-
   guess() {
-    this.currentGuess = Math.ceil((this.min + this.max) / 2);
     return this.currentGuess;
   }
 
-
   lower() {
-    this.max = this.currentGuess - 1;
+    this.max = this.currentGuess; // Уменьшаем максимальное значение
+    this.currentGuess = Math.floor((this.min + this.max) / 2); // Новое предположение
   }
 
-
   greater() {
-    this.min = this.currentGuess + 1;
+    this.min = this.currentGuess; // Увеличиваем минимальное значение
+    this.currentGuess = Math.floor((this.min + this.max) / 2); // Новое предположение
   }
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame; // Экспортируем класс
